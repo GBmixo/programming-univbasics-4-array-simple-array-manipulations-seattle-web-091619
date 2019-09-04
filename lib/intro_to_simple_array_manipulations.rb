0fda35d1fc689003078a3126e4fc1c64f0090e53
decent-token-6050 <garrettburns@mac.com>
2019-09-04 22:36:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,16 @@ end
 def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
+
+def using_insert()
+  
+end
+
+def using_uniq()
+  
+end
+
+def using_flatten()
+  
+end
+

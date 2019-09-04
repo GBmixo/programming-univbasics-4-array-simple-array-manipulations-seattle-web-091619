bad4e8b3950db00b90fda60b2df5200de8dc9589
decent-token-6050 <garrettburns@mac.com>
2019-09-04 22:34:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,4 +22,6 @@ def shift_with_args(ice_cream_brands, integer = 2)
   ice_cream_brands.shift(2)
 end
 
-def using_concat(my_favorite_things, more_faves)
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end

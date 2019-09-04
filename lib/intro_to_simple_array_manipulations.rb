b7d35c5f291fd511c81e8f1e81781ad7bddb0b70
decent-token-6050 <garrettburns@mac.com>
2019-09-04 22:28:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(colors_in_the_rainbow, string)
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(bouroughs_in_nyc, string)
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args (dog_breeds, integer = 2)
+  dog_breeds.pop(integer)
+end
